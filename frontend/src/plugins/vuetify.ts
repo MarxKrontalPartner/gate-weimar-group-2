bae28e8d3552b0e7b2feac1f8b5359b17b461e26
Daniju Babajide <63377821+Bajidaan@users.noreply.github.com>
2025-12-13 19:27:27 +0100

    Add Vuetify plugin configuration

---
+++ frontend/src/plugins/vuetify.ts
@@ -0,0 +1,23 @@
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import { MKPThemes } from '../themes'
+
+export default createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'mkpLightTheme', // <-- FORCE LIGHT THEME
+    themes: {
+      mkpLightTheme: MKPThemes.mkpLightTheme,
+    },
+  },
+  defaults: {
+    VAppBar: {
+      VIcon: {
+        color: 'on-surface',
+      },
+    },
+  },
+})
